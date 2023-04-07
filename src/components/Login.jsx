332447d7e0ea97cbf5ef17c4dbcf777b237f38d4
Louis Rouanet <rouanet_l@ohmytag.fr>
2023-04-07 17:39:10 +0200

    loginpage okay

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,30 +1,24 @@
-// import { AmplifyAuthenticator, AmplifySignIn } from '@aws-amplify/ui-react';
-import { useState } from 'react'
-import '../style/Login.css'
+import React, { useState } from 'react';
+import { AmplifyAuthenticator, AmplifySignIn } from '@aws-amplify/ui-react';
 
-function LoginPage() {
-//   const [isSignedIn, setIsSignedIn] = useState(false);
+export default function LoginPage() {
+  const [isSignedIn, setIsSignedIn] = useState(false);
 
-//   function handleSignIn() {
-//     setIsSignedIn(true);
-//   }
+  function handleSignIn() {
+    setIsSignedIn(true);
+  }
 
   return (
     <div>
+      {isSignedIn ? (
         <div>
-          <h1>You are not signed in!</h1>
+          <h1>You are now signed in!</h1>
         </div>
+      ) : (
+        <AmplifyAuthenticator>
+          <AmplifySignIn slot="sign-in" onSignIn={handleSignIn} />
+        </AmplifyAuthenticator>
+      )}
     </div>
   );
 }
-
-export default LoginPage;
-
-// {isSignedIn ? (
-//     ) 
-//     : (
-//       <div></div>
-//     )}
-// <AmplifyAuthenticator>
-//   <AmplifySignIn slot="sign-in" onSignIn={handleSignIn} />
-// </AmplifyAuthenticator>
